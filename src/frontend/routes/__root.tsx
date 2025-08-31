3133adac6b19063a002c9059718d1067a7eb2faa
Kristofer Lund <kristofer@fmckl.se>
2025-08-31 07:23:09 +0200

    Styling

--- src/frontend/routes/__root.tsx
+++ src/frontend/routes/__root.tsx
@@ -1,15 +1,14 @@
 import reactLogo from "../assets/react.svg";
 import viteLogo from "../assets/vite.svg";
 import icLogo from "../assets/ic.svg";
-import { createRootRoute, Link, Outlet } from "@tanstack/react-router";
+import { createRootRoute, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
-import GithubLink from "@/components/github-link";
 
 export const Route = createRootRoute({
   component: () => (
     <main className="dark">
       <div className="flex flex-col gap-14 items-center w-[400px]">
-        <div className="flex gap-10">
+        <div className="flex gap-10 mt-20">
           <a
             href="https://internetcomputer.org"
             target="_blank"
@@ -24,22 +23,15 @@ export const Route = createRootRoute({
             <img src={reactLogo} alt="React logo" className="h-20" />
           </a>
         </div>
-        <h1>ICP Starter</h1>
-        <h2 className="text-center">
+        <h1>Running SQLite on ICP</h1>
+        <div className="text-center text-white">
+          A fullstack application using the latest of the greatest. SQLite
+          in the backend canister plus these frameworks in the frontend:
           React + TypeScript + Vite + Tailwind + Shadcn/UI + Tanstack
-          Query/Router
-        </h2>
-        <div className="flex gap-5 text-white items-center">
-          <Link to="/" className="hover:text-white/70">
-            /index
-          </Link>
-          <Link to="/add-person" className="hover:text-white/70">
-            /add-person
-          </Link>
-          <Link to="/about" className="hover:text-white/70">
-            /about
-          </Link>
-          <GithubLink />
+          Query/Router.
+        </div>
+        <div className="text-center text-white">
+          Fork it on GitHub: <a href="https://github.com/kristoferlund/ic-sqlite-react-demo" target="_blank" rel="noreferrer" className="underline">ic-sqlite-react-demo</a>
         </div>
         <Outlet />
         <TanStackRouterDevtools />
